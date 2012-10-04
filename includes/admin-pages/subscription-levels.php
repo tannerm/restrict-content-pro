373e5fc44a4965cc162aec4a3345e10b602e7249
corsonr <remicorson@gmail.com>
2012-10-04 10:52:24 +0300

    Access level show " None selected='selected'"
    When you create a new subscription and set "Access Level" to "none".
    Once you click on "Add Membership Level", in the subscription list,
    in "Access Level" column it shows " None selected='selected' ".
    
    This bug doesn't occur when you edit the subscription.

--- includes/admin-pages/subscription-levels.php
+++ includes/admin-pages/subscription-levels.php
@@ -112,7 +112,7 @@ function rcp_member_levels_page()
 									<?php
 									$access_levels = rcp_get_access_levels();
 									foreach( $access_levels as $access ) {
-										echo '<option value="' . $access . ' ' . selected($accessi, $level->level, false) . '">' . $access . '</option>';
+										echo '<option value="' . $access . ' ' . selected($access, $level->level, false) . '">' . $access . '</option>';
 									}	
 									?>
 								</select>
