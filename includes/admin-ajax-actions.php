0e771a35399a8dd270bd661ce90c8ed19b3e7bff
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-06 12:58:16 -0500

    Improved code formatting for admin-ajax-actions.php

--- includes/admin-ajax-actions.php
+++ includes/admin-ajax-actions.php
@@ -2,26 +2,26 @@
 
 // retrieves the expiration date of a subscription level
 function rcp_ajax_get_subscription_expiration() {
-	if(isset($_POST['subscription_level'])) {
+	if( isset( $_POST['subscription_level'] ) ) {
 		$level_id = $_POST['subscription_level'];
-		$expiration = rcp_calculate_subscription_expiration($level_id);
+		$expiration = rcp_calculate_subscription_expiration( $level_id );
 		echo $expiration;
 	}
 	die();
 }
-add_action('wp_ajax_rcp_get_subscription_expiration', 'rcp_ajax_get_subscription_expiration');
+add_action( 'wp_ajax_rcp_get_subscription_expiration', 'rcp_ajax_get_subscription_expiration' );
 
 // processes the ajax re-ordering request
 function rcp_update_subscription_order() {
-	if(isset($_POST['recordsArray'])) {
+	if( isset( $_POST['recordsArray'] ) ) {
 		global $wpdb, $rcp_db_name;
 		$updateRecordsArray = $_POST['recordsArray'];
 		$listingCounter = 1;
-		foreach ($updateRecordsArray as $recordIDValue) {
+		foreach ( $updateRecordsArray as $recordIDValue ) {
 			$new_order = $wpdb->update(
 				$rcp_db_name, 
-				array('list_order' => $listingCounter ), 
-				array('id' => $recordIDValue),
+				array('list_order' 	=> $listingCounter ), 
+				array('id' 			=> $recordIDValue),
 				array('%d')
 			);
 			$listingCounter++;
@@ -30,16 +30,15 @@ function rcp_update_subscription_order() {
 		delete_transient('rcp_subscription_levels');
 	}
 	die();
-	
 }
-add_action('wp_ajax_update-subscription-order', 'rcp_update_subscription_order');
+add_action( 'wp_ajax_update-subscription-order', 'rcp_update_subscription_order' );
 
 // retrieves a list of users via live search
 function rcp_search_users() {
 
 	if( wp_verify_nonce( $_POST['rcp_nonce'], 'rcp_member_nonce' ) ) {
 		
-		$search_query = trim($_POST['user_name']);
+		$search_query = trim( $_POST['user_name'] );
 
 		$found_users = get_users( array(
 				'number' => 9999,
@@ -49,7 +48,7 @@ function rcp_search_users() {
 		
 		if( $found_users ) {
 			$user_list = '<ul>';
-				foreach($found_users as $user) {
+				foreach( $found_users as $user ) {
 					$user_list .= '<li><a href="#" data-login="' . $user->user_login . '">' . $user->user_login . '</a></li>';	
 				}
 			$user_list .= '</ul>';
@@ -57,10 +56,10 @@ function rcp_search_users() {
 			echo json_encode( array( 'results' => $user_list, 'id' => 'found' ) );		
 			
 		} else {
-			echo json_encode( array( 'msg' => __('No users found', 'rcp'), 'results' => 'none', 'id' => 'fail' ) );	
+			echo json_encode( array( 'msg' => __( 'No users found', 'rcp' ), 'results' => 'none', 'id' => 'fail' ) );	
 		}		
 		
 	}	
 	die();
 }
-add_action('wp_ajax_rcp_search_users', 'rcp_search_users');
+add_action( 'wp_ajax_rcp_search_users', 'rcp_search_users' );
