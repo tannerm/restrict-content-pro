867e730c8c4d160f6b636b03456f434bca59b132
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-06 13:01:31 -0500

    Added esc_url() to admin notice

--- includes/admin-notices.php
+++ includes/admin-notices.php
@@ -11,7 +11,7 @@ add_action( 'admin_notices', 'rcp_first_activation_notice' );
 
 function rcp_upgrade_needed_notice() {
 	if( rcp_check_if_upgrade_needed() ) {
-		echo '<div class="error"><p>' . __( 'The Restrict Content Pro database needs updated: ', 'rcp' ) . ' ' . '<a href="' . dd_query_arg( 'rcp-action', 'upgrade', admin_url() ) . '">' . __( 'upgrade now', 'rcp' ) . '</a></p></div>';
+		echo '<div class="error"><p>' . __( 'The Restrict Content Pro database needs updated: ', 'rcp' ) . ' ' . '<a href="' . esc_url( add_query_arg( 'rcp-action', 'upgrade', admin_url() ) ) . '">' . __( 'upgrade now', 'rcp' ) . '</a></p></div>';
 	}
 	if( isset( $_GET['rcp-db'] ) && $_GET['rcp-db'] == 'updated' ) {
 		echo '<div class="updated fade"><p>' . __( 'The Restrict Content Pro database has been updated', 'rcp' ) . '</p></div>';
