36fa1f083c830c43ad5461707af5e4aea008bd3f
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-06 15:22:10 -0500

    Escaped URLs and replaced get_bloginfo() with admin_url()

--- includes/user-page-columns.php
+++ includes/user-page-columns.php
@@ -15,12 +15,12 @@ function rcp_show_user_columns( $value, $column_name, $user_id ) {
 		return rcp_get_subscription( $user_id );
 	}
 	if ( 'rcp_links' == $column_name ) {
-		$page = get_bloginfo('wpurl') . '/wp-admin/admin.php?page=rcp-members';
+		$page = admin_url( '/admin.php?page=rcp-members' );
 		if( rcp_is_active( $user_id ) ) {
-			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __( 'Edit Subscription', 'rcp' ) . '</a>';
-			$links .= ' | <a href="' . $page . '&view_member=' . $user_id . '">' . __( 'View Details','rcp' ) . '</a>';
+			$links = '<a href="' . esc_url( $page ) . '&edit_member=' . esc_attr( absint( $user_id ) ) . '">' . __( 'Edit Subscription', 'rcp' ) . '</a>';
+			$links .= ' | <a href="' . esc_url( $page ) . '&view_member=' . esc_attr( absint( $user_id ) ) . '">' . __( 'View Details','rcp' ) . '</a>';
 		} else {
-			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __( 'Add Subscription', 'rcp' ) . '</a>';
+			$links = '<a href="' . esc_url( $page ) . '&edit_member=' . esc_attr( absint( $user_id ) ) . '">' . __( 'Add Subscription', 'rcp' ) . '</a>';
 		}
 		return $links;
 	}
