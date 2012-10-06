23c3ff6b531afc0b5e95b1e98d515f136e7fa597
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-06 14:09:37 -0500

    Added if enabled check to captcha validation

--- includes/captcha-functions.php
+++ includes/captcha-functions.php
@@ -15,17 +15,19 @@ add_action( 'rcp_before_registration_submit_field', 'rcp_show_captcha', 100 );
 
 function rcp_validate_captcha() {
 	global $rcp_options;
-	/* validate recaptcha, if enabled */
-	$privatekey = trim( $rcp_options['recaptcha_private_key'] );
-	$resp = recaptcha_check_answer(
-		$privatekey,
-		$_SERVER["REMOTE_ADDR"],
-		$_POST["recaptcha_challenge_field"],
-		$_POST["recaptcha_response_field"]
-	);
-	if ( !$resp->is_valid ) {
-		// recaptcha is incorrect
-		rcp_errors()->add( 'invalid_recaptcha', __( 'The words/numbers you entered did not match the reCaptcha', 'rcp' ) );
+	if( isset( $rcp_options['enable_recaptcha'] ) ) {
+		/* validate recaptcha, if enabled */
+		$privatekey = trim( $rcp_options['recaptcha_private_key'] );
+		$resp = recaptcha_check_answer(
+			$privatekey,
+			$_SERVER["REMOTE_ADDR"],
+			$_POST["recaptcha_challenge_field"],
+			$_POST["recaptcha_response_field"]
+		);
+		if ( !$resp->is_valid ) {
+			// recaptcha is incorrect
+			rcp_errors()->add( 'invalid_recaptcha', __( 'The words/numbers you entered did not match the reCaptcha', 'rcp' ) );
+		}
 	}
 }
 add_action( 'rcp_form_errors', 'rcp_validate_captcha' );
