2128abd7198a29afde6100df1e5a979fb82032f9
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-06 15:20:57 -0500

    Improved code formatting for user page columns

--- includes/user-page-columns.php
+++ includes/user-page-columns.php
@@ -1,29 +1,29 @@
 <?php 
 
-function rcp_add_user_columns($columns) {
-	$columns['rcp_subscription'] = 'Subscription';
-    $columns['rcp_status'] = 'Status';
-	$columns['rcp_links'] = 'Actions';
+function rcp_add_user_columns( $columns ) {
+	$columns['rcp_subscription'] 	= 'Subscription';
+    $columns['rcp_status'] 			= 'Status';
+	$columns['rcp_links'] 			= 'Actions';
     return $columns;
 }
-add_filter('manage_users_columns', 'rcp_add_user_columns');
+add_filter( 'manage_users_columns', 'rcp_add_user_columns' );
  
-function rcp_show_user_columns($value, $column_name, $user_id) {
+function rcp_show_user_columns( $value, $column_name, $user_id ) {
 	if ( 'rcp_status' == $column_name )
-		return rcp_get_status($user_id);
+		return rcp_get_status( $user_id );
 	if ( 'rcp_subscription' == $column_name ) {
-		return rcp_get_subscription($user_id);
+		return rcp_get_subscription( $user_id );
 	}
 	if ( 'rcp_links' == $column_name ) {
 		$page = get_bloginfo('wpurl') . '/wp-admin/admin.php?page=rcp-members';
-		if(rcp_is_active($user_id)) {
-			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __('Edit Subscription', 'rcp') . '</a>';
-			$links .= ' | <a href="' . $page . '&view_member=' . $user_id . '">' . __('View Details','rcp') . '</a>';
+		if( rcp_is_active( $user_id ) ) {
+			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __( 'Edit Subscription', 'rcp' ) . '</a>';
+			$links .= ' | <a href="' . $page . '&view_member=' . $user_id . '">' . __( 'View Details','rcp' ) . '</a>';
 		} else {
-			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __('Add Subscription', 'rcp') . '</a>';
+			$links = '<a href="' . $page . '&edit_member=' . $user_id . '">' . __( 'Add Subscription', 'rcp' ) . '</a>';
 		}
 		return $links;
 	}
 	return $value;
 }
-add_filter('manage_users_custom_column',  'rcp_show_user_columns', 100, 3);
+add_filter( 'manage_users_custom_column',  'rcp_show_user_columns', 100, 3 );
