84e440d72981f861d8da9624173c4efba12025aa
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-06 14:56:58 -0600

    Fixed placeholder in rcp_check_for_existing_payment()

--- includes/payments.php
+++ includes/payments.php
@@ -137,7 +137,7 @@ function rcp_check_for_existing_payment( $type, $date, $subscription_key ) {
 
 	global $wpdb, $rcp_payments_db_name;
 
-	if( $wpdb->get_results( $wpdb->prepare("SELECT id FROM " . $rcp_payments_db_name . " WHERE `date`='%d' AND `subscription_key`='%s' AND `payment_type`='%s';", $date, $subscription_key, $type ) ) )
+	if( $wpdb->get_results( $wpdb->prepare("SELECT id FROM " . $rcp_payments_db_name . " WHERE `date`='%s' AND `subscription_key`='%s' AND `payment_type`='%s';", $date, $subscription_key, $type ) ) )
 		return true; // this payment already exists
 
 	return false; // this payment doesn't exist
