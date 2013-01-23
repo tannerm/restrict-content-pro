fe19dc6b14cb12a4bde3ae8bc7d20704a4db3053
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-23 13:38:43 -0600

    Load admin styles on logs page

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,7 +3,7 @@
 function rcp_admin_scripts($hook) {
 	global $rcp_members_page, $rcp_subscriptions_page, $rcp_discounts_page, $rcp_payments_page, $rcp_settings_page, $rcp_export_page, $rcp_help_page;
 	$pages = array( $rcp_members_page, $rcp_subscriptions_page, $rcp_discounts_page, $rcp_payments_page, $rcp_settings_page, $rcp_export_page, $rcp_help_page );
-	
+
 	if( in_array( $hook, $pages ) ) {
 		wp_enqueue_script( 'jquery-ui-sortable' );
 		wp_enqueue_script( 'jquery-ui-datepicker' );
@@ -11,26 +11,26 @@ function rcp_admin_scripts($hook) {
 		wp_enqueue_script( 'rcp-admin-scripts',  RCP_PLUGIN_URL . 'includes/js/admin-scripts.js' );
 	}
 	if( $hook == $rcp_help_page ) {
-		wp_enqueue_style( 'jquery-snippet',  RCP_PLUGIN_URL . 'includes/css/jquery.snippet.min.css' );	
-		wp_enqueue_script( 'jquery-snippet',  RCP_PLUGIN_URL . 'includes/js/jquery.snippet.min.js' );	
+		wp_enqueue_style( 'jquery-snippet',  RCP_PLUGIN_URL . 'includes/css/jquery.snippet.min.css' );
+		wp_enqueue_script( 'jquery-snippet',  RCP_PLUGIN_URL . 'includes/js/jquery.snippet.min.js' );
 	}
 	if( $hook == $rcp_members_page ) {
 		wp_localize_script( 'rcp-admin-scripts', 'rcp_member_vars', array(
 				'rcp_member_nonce' => wp_create_nonce( 'rcp_member_nonce' )
 			)
-		); 	
+		);
 	}
 }
 add_action( 'admin_enqueue_scripts', 'rcp_admin_scripts' );
 
 function rcp_admin_styles( $hook ) {
-	global $rcp_members_page, $rcp_subscriptions_page, $rcp_discounts_page, $rcp_payments_page, $rcp_settings_page, $rcp_export_page, $rcp_help_page;
+	global $rcp_members_page, $rcp_subscriptions_page, $rcp_discounts_page, $rcp_payments_page, $rcp_settings_page, $rcp_export_page, $rcp_logs_page, $rcp_help_page;
 	$pages = array(
-		$rcp_members_page, 
-		$rcp_subscriptions_page, 
-		$rcp_discounts_page, 
-		$rcp_payments_page, 
-		$rcp_settings_page, 
+		$rcp_members_page,
+		$rcp_subscriptions_page,
+		$rcp_discounts_page,
+		$rcp_payments_page,
+		$rcp_settings_page,
 		$rcp_export_page,
 		$rcp_logs_page,
 		$rcp_help_page,
@@ -38,7 +38,7 @@ function rcp_admin_styles( $hook ) {
 		'edit.php',
 		'post-new.php'
 	);
-	
+
 	if( in_array( $hook, $pages ) ) {
 		wp_enqueue_style( 'datepicker',  RCP_PLUGIN_URL . 'includes/css/datepicker.css' );
 		wp_enqueue_style( 'rcp-admin',  RCP_PLUGIN_URL . 'includes/css/admin-styles.css' );
@@ -59,11 +59,11 @@ function rcp_register_scripts() {
 	wp_register_script( 'jquery-validate',  RCP_PLUGIN_URL . 'includes/js/jquery.validate.min.js', array('jquery') );
 }
 add_action( 'init', 'rcp_register_scripts' );
- 
+
 // load our form css
 function rcp_print_css() {
 	global $rcp_load_css, $rcp_options;
- 
+
 	// this variable is set to TRUE if the short code is used on a page/post
 	if ( ! $rcp_load_css || ( isset( $rcp_options['disable_css'] ) && $rcp_options['disable_css'] ) )
 		return; // this means that neither short code is present, so we get out of here
@@ -75,7 +75,7 @@ add_action( 'wp_footer', 'rcp_print_css' );
 // load our form scripts
 function rcp_print_scripts() {
 	global $rcp_load_scripts, $rcp_options;
- 
+
 	// this variable is set to TRUE if the short code is used on a page/post
 	if ( ! $rcp_load_scripts )
 		return; // this means that neither short code is present, so we get out of here
@@ -84,12 +84,12 @@ function rcp_print_scripts() {
 		$validate = 'true';
 	else
 		$validate = 'false';
-	
-	wp_localize_script('rcp-scripts', 'rcp_script_options', 
-		array( 
+
+	wp_localize_script('rcp-scripts', 'rcp_script_options',
+		array(
 			'validate' 	=> $validate,
 			'ajaxurl' 	=> admin_url( 'admin-ajax.php' )
-		) 
+		)
 	);
 	wp_print_scripts( 'rcp-scripts' );
 	wp_print_scripts( 'jquery-validate' );
