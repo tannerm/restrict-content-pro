8b26fdb8f4d278ac656fa4ec422f45fd191e1134
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-21 22:32:22 -0600

    Added base class with function shells for new RCP_Discounts. #42

---
+++ includes/class-rcp-discounts.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * RCP Discounts class
+ *
+ * This class handles querying, inserting, updating, and removing discounts
+ * Also includes other discount helper functions
+ *
+ * @since 1.5
+*/
+
+
+class RCP_Discounts {
+
+	/**
+	 * Holds the name of our discounts database table
+	 *
+	 * @access  private
+	 * @since   1.5
+	*/
+
+	private $db_name;
+
+
+	/**
+	 * Holds the version number of our discounts database table
+	 *
+	 * @access  private
+	 * @since   1.5
+	*/
+
+	private $db_version;
+
+
+
+	function __construct() {
+
+		$this->db_name    = rcp_get_discounts_db_name();
+		$this->db_version = '1.1';
+
+	}
+
+
+	public function get_discounts( $args = array() ) {
+
+	}
+
+
+	public function get_discount( $discount_id = 0 ) {
+
+	}
+
+	public function get_by( $field = 'code', $value = '' ) {
+
+	}
+
+
+	public function get_status( $discount_id = 0 ) {
+
+	}
+
+	public function get_amount( $discount_id = 0 ) {
+
+	}
+
+
+	public function get_uses( $discount_id = 0 ) {
+
+	}
+
+	public function get_expiration( $discount_id = 0 ) {
+
+	}
+
+	public function get_type( $discount_id = 0 ) {
+
+	}
+
+
+	public function insert( $args = array() ) {
+
+	}
+
+	public function update( $discount_id = 0, $args = array() ) {
+
+	}
+
+	public function delete( $discount_id = 0 ) {
+
+	}
+
+	public function is_maxed_out( $discount_id = 0 ) {
+
+	}
+
+	public function is_expired( $discount_id = 0 ) {
+
+	}
+
+	public function add_to_user( $user_id = 0, $discount_code = '' ) {
+
+	}
+
+	public function user_has_used( $user_id = 0, $discount_code = '' ) {
+
+	}
+
+	public function increase_use( $discount_id = 0 ) {
+
+	}
+
+	public function count_uses( $discount_id = 0 ) {
+
+	}
+
+	public function format_discount( $amount = '', $type = '' ) {
+
+	}
+
+	public function calc_discounted_price( $base_price = '', $discount_amount = '', $type = '%' ) {
+
+	}
+
+}
