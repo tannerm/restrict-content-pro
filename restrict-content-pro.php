3db039181697da3517d97001bf902fb6a69881ca
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-03 22:24:25 -0600

    Include RCP_Discounts. #42

--- restrict-content-pro.php
+++ restrict-content-pro.php
@@ -135,6 +135,7 @@ add_action( 'init', 'rcp_load_textdomain' );
 
 // global includes
 include( RCP_PLUGIN_DIR . 'includes/class-rcp-payments.php' );
+include( RCP_PLUGIN_DIR . 'includes/class-rcp-discounts.php' );
 include( RCP_PLUGIN_DIR . 'includes/gateways/paypal/paypal.php' );
 include( RCP_PLUGIN_DIR . 'includes/misc-functions.php' );
 include( RCP_PLUGIN_DIR . 'includes/scripts.php' );
