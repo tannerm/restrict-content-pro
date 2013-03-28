38ebabcfc7fe626c5b43e6a96bd736b6fad3b9eb
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-28 11:14:43 -0500

    Extracted login functions to new file. #53

--- includes/handle-registration-login.php
+++ includes/handle-registration-login.php
@@ -205,61 +205,6 @@ function rcp_process_registration() {
 }
 add_action( 'init', 'rcp_process_registration', 100 );
 
-// logs the specified user in
-function rcp_login_user_in( $user_id, $user_login, $user_pass ) {
-	$user = get_userdata( $user_id );
-	if( ! $user )
-		return;
-	wp_set_auth_cookie( $user_id );
-	wp_set_current_user( $user_id, $user_login );
-	do_action( 'wp_login', $user_login, $user );
-}
-
-// logs a member in after submitting a form
-function rcp_process_login_form() {
-
-	if( isset( $_POST['rcp_action'] ) && $_POST['rcp_action'] == 'login' ) {
-		if( isset( $_POST['rcp_login_nonce'] ) && wp_verify_nonce( $_POST['rcp_login_nonce'], 'rcp-login-nonce' ) ) {
-
-			// this returns the user ID and other info from the user name
-			$user = get_user_by( 'login', $_POST['rcp_user_login'] );
-
-			do_action( 'rcp_before_form_errors', $_POST );
-
-			if( !$user ) {
-				// if the user name doesn't exist
-				rcp_errors()->add( 'empty_username', __( 'Invalid username', 'rcp' ), 'login' );
-			}
-
-			if( !isset( $_POST['rcp_user_pass'] ) || $_POST['rcp_user_pass'] == '') {
-				// if no password was entered
-				rcp_errors()->add( 'empty_password', __( 'Please enter a password', 'rcp' ), 'login' );
-			}
-
-			if( $user ) {
-				// check the user's login with their password
-				if( !wp_check_password( $_POST['rcp_user_pass'], $user->user_pass, $user->ID ) ) {
-					// if the password is incorrect for the specified user
-					rcp_errors()->add( 'empty_password', __( 'Incorrect password', 'rcp' ), 'login' );
-				}
-			}
-			do_action( 'rcp_login_form_errors', $_POST );
-
-			// retrieve all error messages
-			$errors = rcp_errors()->get_error_messages();
-
-			// only log the user in if there are no errors
-			if( empty( $errors ) ) {
-
-				rcp_login_user_in( $user->ID, $_POST['rcp_user_login'], $_POST['rcp_user_pass'] );
-
-				// redirect the user back to the page they were previously on
-				wp_redirect( $_POST['rcp_redirect'] ); exit;
-			}
-		}
-	}
-}
-add_action('init', 'rcp_process_login_form');
 
 function rcp_reset_password() {
 	// reset a users password
