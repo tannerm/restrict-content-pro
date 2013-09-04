ce308c4f9143dc98601226669dfb39bb7392be13
Pippin Williamson <pippin@pippinsplugins.com>
2013-09-04 11:48:15 -0500

    Improve RSS feed filters

--- includes/feed-functions.php
+++ includes/feed-functions.php
@@ -4,18 +4,22 @@
 * Restrict Content Feed Functions
 *******************************************/
 
-function rcp_filter_feed_posts($content) {
+function rcp_filter_feed_posts( $content ) {
 	global $rcp_options;
+
+	if( ! is_feed() )
+		return $content;
+
 	$hide_from_feed = get_post_meta( get_the_ID(), 'rcp_hide_from_feed', true );
 	if ( $hide_from_feed == 'on' ) {
 		if( rcp_is_paid_content( $post_id ) ) {
-			return $rcp_options['paid_message'];
+			return rcp_format_teaser( $rcp_options['paid_message'] );
 		} else {
-			return $rcp_options['free_message'];
+			return rcp_format_teaser( $rcp_options['free_message'] );
 		}
 	}
 	return $content;
-	
+
 }
-add_action( 'the_excerpt_rss', 'rcp_filter_feed_posts' );
-add_action( 'the_content_rss', 'rcp_filter_feed_posts' );
+add_action( 'the_excerpt', 'rcp_filter_feed_posts' );
+add_action( 'the_content', 'rcp_filter_feed_posts' );
