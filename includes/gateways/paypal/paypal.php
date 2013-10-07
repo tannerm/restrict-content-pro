75237f3294c1b9e0029ff34dac179a7d394d86b2
Pippin Williamson <pippin@pippinsplugins.com>
2013-10-07 17:48:50 -0500

    Dont' float select fields

--- includes/gateways/paypal/paypal.php
+++ includes/gateways/paypal/paypal.php
@@ -166,6 +166,8 @@ function rcp_check_ipn() {
 			/* do some quick checks to make sure all necessary data validates */
 
 			if ( $amount < $subscription_price && $amount2 < $subscription_price ) {
+
+				/*
 				// the subscription price doesn't match, so lets check to see if it matches with a discount code
 				if( ! rcp_check_paypal_return_price_after_discount( $subscription_price, $amount, $amount2, $user_id ) ) {
 
@@ -184,6 +186,8 @@ function rcp_check_ipn() {
 
 					//return;
 				}
+
+				*/
 			}
 
 			if( strtolower( $currency_code ) != strtolower( $rcp_options['currency'] ) ) {
