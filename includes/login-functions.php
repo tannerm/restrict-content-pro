76d7232436184964f87a5eb7181cb23fce3292a8
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-19 16:22:45 -0500

    Cleanup rcp_process_login_form(). #99

--- includes/login-functions.php
+++ includes/login-functions.php
@@ -39,47 +39,51 @@ function rcp_login_user_in( $user_id, $user_login, $remember = false ) {
  */
 function rcp_process_login_form() {
 
-	if( isset( $_POST['rcp_action'] ) && $_POST['rcp_action'] == 'login' ) {
-		if( isset( $_POST['rcp_login_nonce'] ) && wp_verify_nonce( $_POST['rcp_login_nonce'], 'rcp-login-nonce' ) ) {
+	if( ! isset( $_POST['rcp_action'] ) || 'login' != $_POST['rcp_action'] ) {
+		return;
+	}
 
-			// this returns the user ID and other info from the user name
-			$user = get_user_by( 'login', $_POST['rcp_user_login'] );
+	if( ! isset( $_POST['rcp_login_nonce'] ) || ! wp_verify_nonce( $_POST['rcp_login_nonce'], 'rcp-login-nonce' ) ) {
+		return;
+	}
 
-			do_action( 'rcp_before_form_errors', $_POST );
+	// this returns the user ID and other info from the user name
+	$user = get_user_by( 'login', $_POST['rcp_user_login'] );
 
-			if( !$user ) {
-				// if the user name doesn't exist
-				rcp_errors()->add( 'empty_username', __( 'Invalid username', 'rcp' ), 'login' );
-			}
+	do_action( 'rcp_before_form_errors', $_POST );
 
-			if( !isset( $_POST['rcp_user_pass'] ) || $_POST['rcp_user_pass'] == '') {
-				// if no password was entered
-				rcp_errors()->add( 'empty_password', __( 'Please enter a password', 'rcp' ), 'login' );
-			}
+	if( !$user ) {
+		// if the user name doesn't exist
+		rcp_errors()->add( 'empty_username', __( 'Invalid username', 'rcp' ), 'login' );
+	}
 
-			if( $user ) {
-				// check the user's login with their password
-				if( !wp_check_password( $_POST['rcp_user_pass'], $user->user_pass, $user->ID ) ) {
-					// if the password is incorrect for the specified user
-					rcp_errors()->add( 'empty_password', __( 'Incorrect password', 'rcp' ), 'login' );
-				}
-			}
-			do_action( 'rcp_login_form_errors', $_POST );
+	if( !isset( $_POST['rcp_user_pass'] ) || $_POST['rcp_user_pass'] == '') {
+		// if no password was entered
+		rcp_errors()->add( 'empty_password', __( 'Please enter a password', 'rcp' ), 'login' );
+	}
 
-			// retrieve all error messages
-			$errors = rcp_errors()->get_error_messages();
-
-			// only log the user in if there are no errors
-			if( empty( $errors ) ) {
-
-				$remember = isset( $_POST['rcp_user_remember'] );
-
-				rcp_login_user_in( $user->ID, $_POST['rcp_user_login'], $remember );
-
-				// redirect the user back to the page they were previously on
-				wp_redirect( $_POST['rcp_redirect'] ); exit;
-			}
+	if( $user ) {
+		// check the user's login with their password
+		if( !wp_check_password( $_POST['rcp_user_pass'], $user->user_pass, $user->ID ) ) {
+			// if the password is incorrect for the specified user
+			rcp_errors()->add( 'empty_password', __( 'Incorrect password', 'rcp' ), 'login' );
 		}
+	}
+
+	do_action( 'rcp_login_form_errors', $_POST );
+
+	// retrieve all error messages
+	$errors = rcp_errors()->get_error_messages();
+
+	// only log the user in if there are no errors
+	if( empty( $errors ) ) {
+
+		$remember = isset( $_POST['rcp_user_remember'] );
+
+		rcp_login_user_in( $user->ID, $_POST['rcp_user_login'], $remember );
+
+		// redirect the user back to the page they were previously on
+		wp_redirect( $_POST['rcp_redirect'] ); exit;
 	}
 }
 add_action('init', 'rcp_process_login_form');
