7cbaf2c7d7e42953a076d1b67eb7d3c1661e8715
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-02 15:40:30 -0500

    Fix order of events so that member notes get logged on expiration changes

--- includes/process-data.php
+++ includes/process-data.php
@@ -92,7 +92,6 @@ function rcp_process_data() {
 			$level      = absint( $_POST['level'] );
 			$expiration = isset( $_POST['expiration'] ) ? sanitize_text_field( $_POST['expiration'] ) : 'none';
 
-			rcp_set_expiration_date( $user_id, $expiration );
 
 			if( isset( $_POST['level'] ) ) update_user_meta( $user_id, 'rcp_subscription_level', $level );
 			if( isset( $_POST['recurring'] ) ) {
@@ -104,6 +103,8 @@ function rcp_process_data() {
 			if( isset( $_POST['notes'] ) ) update_user_meta( $user_id, 'rcp_notes', wp_kses( $_POST['notes'], array() ) );
 			if( isset( $_POST['status'] ) ) rcp_set_status( $user_id, $status );
 
+			rcp_set_expiration_date( $user_id, $expiration );
+			
 			do_action( 'rcp_edit_member', $user_id );
 
 			wp_redirect( admin_url( 'admin.php?page=rcp-members&edit_member=' . $user_id . '&rcp_message=user_updated' ) ); exit;
