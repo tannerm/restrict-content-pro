c647dfd7d3487b94da5aeeeb30ac29235212822e
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-13 14:21:17 -0500

    Move change password function to member-functions

--- includes/member-functions.php
+++ includes/member-functions.php
@@ -753,3 +753,52 @@ function rcp_process_profile_editor_updates() {
 	}
 }
 add_action( 'init', 'rcp_process_profile_editor_updates' );
+
+/**
+ * Change a user password
+ *
+ * @access      public
+ * @since       1.0
+ */
+function rcp_change_password() {
+	// reset a users password
+	if( isset( $_POST['rcp_action'] ) && $_POST['rcp_action'] == 'reset-password' ) {
+
+		global $user_ID;
+
+		if( !is_user_logged_in() )
+			return;
+
+		if( wp_verify_nonce( $_POST['rcp_password_nonce'], 'rcp-password-nonce' ) ) {
+
+			do_action( 'rcp_before_password_form_errors', $_POST );
+
+			if( $_POST['rcp_user_pass'] == '' || $_POST['rcp_user_pass_confirm'] == '' ) {
+				// password(s) field empty
+				rcp_errors()->add( 'password_empty', __( 'Please enter a password, and confirm it', 'rcp' ), 'password' );
+			}
+			if( $_POST['rcp_user_pass'] != $_POST['rcp_user_pass_confirm'] ) {
+				// passwords do not match
+				rcp_errors()->add( 'password_mismatch', __( 'Passwords do not match', 'rcp' ), 'password' );
+			}
+
+			do_action( 'rcp_password_form_errors', $_POST );
+
+			// retrieve all error messages, if any
+			$errors = rcp_errors()->get_error_messages();
+
+			if( empty( $errors ) ) {
+				// change the password here
+				$user_data = array(
+					'ID' 		=> $user_ID,
+					'user_pass' => $_POST['rcp_user_pass']
+				);
+				wp_update_user( $user_data );
+				// send password change email here (if WP doesn't)
+				wp_redirect( add_query_arg( 'password-reset', 'true', $_POST['rcp_redirect'] ) );
+				exit;
+			}
+		}
+	}
+}
+add_action( 'init', 'rcp_change_password' );
