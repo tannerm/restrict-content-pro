739579f3f6c64e6ed7df37144862af521f6506cb
Pippin Williamson <pippin@pippinsplugins.com>
2015-01-25 11:41:28 -0700

    Add JS for processing registration form. #120

--- includes/js/register.js
+++ includes/js/register.js
@@ -99,6 +99,36 @@ jQuery(document).ready(function($) {
 
 	});
 
+	$(document).on('click', '#rcp_registration_form #rcp_submit', function(e) {
+
+		var submission_form = document.getElementById('rcp_registration_form');
+
+		if( typeof submission_form.checkValidity === "function" && false === submission_form.checkValidity() ) {
+			return;
+		}
+
+		e.preventDefault();
+
+		var submit_register_text = $(this).val();
+
+		$('#rcp_submit').val( rcp_script_options.pleasewait );
+		$(this).after('<span class="rcp-submit-ajax"><i class="rcp-icon-spinner rcp-icon-spin"></i></span>');
+
+		$.post( rcp_script_options.ajaxurl, $('#rcp_registration_form').serialize() + '&action=rcp_process_register_form&rcp_ajax=true', function(response) {
+
+			$('.rcp-submit-ajax').remove();
+			$('.rcp_message.error').remove();
+			if ( response.success ) {
+				$(submission_form).submit();
+			} else {
+				$('#rcp_submit').val( submit_register_text );
+				$('#rcp_submit').before( response.data.errors );
+				$('#rcp_register_nonce').val( response.data.nonce );
+			}
+		});
+
+	});
+
 });
 
 
