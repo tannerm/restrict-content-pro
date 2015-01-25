6015d25b26efd4bec609f5e4c58befb98a779fd1
Pippin Williamson <pippin@pippinsplugins.com>
2015-01-25 11:40:53 -0700

    Introduce rcp_get_error_messages_html() and add PHPdoc to error tracking. #120

--- includes/error-tracking.php
+++ includes/error-tracking.php
@@ -1,28 +1,61 @@
 <?php
 
-// displays error messages from form submissions
-function rcp_show_error_messages( $error_id = '' ) {
-	if( $codes = rcp_errors()->get_error_codes() ) {
-		do_action( 'rcp_errors_before' );
-		echo '<div class="rcp_message error">';
-		    // Loop error codes and display errors
-		   foreach( $codes as $code ) {
-		   		if( rcp_errors()->get_error_data( $code ) == $error_id ) {
-
-			        $message = rcp_errors()->get_error_message($code);
-
-			        do_action( 'rcp_error_before' );
-			        echo '<p class="rcp_error ' . $code . '"><span>' . $message . '</span></p>';
-			        do_action( 'rcp_error_after' );
-		    	}
-		    }
-		echo '</div>';
-		do_action( 'rcp_errors_after' );
-	}
-}
-
-// used for tracking error messages
-function rcp_errors(){
+/**
+ * Stores error messages
+ *
+ * @access      public
+ * @since       1.0
+ */
+function rcp_errors() {
     static $wp_error; // Will hold global variable safely
     return isset( $wp_error ) ? $wp_error : ( $wp_error = new WP_Error( null, null, null ) );
+}
+
+/**
+ * Retrieves the HTML for error messages
+ *
+ * @access      public
+ * @since       2.1
+ */
+function rcp_get_error_messages_html( $error_id = '' ) {
+
+	$html   = '';
+	$errors = rcp_errors()->get_error_codes();
+
+	if( $errors ) {
+		
+		$html .= '<div class="rcp_message error">';
+		// Loop error codes and display errors
+		foreach( $errors as $code ) {
+
+			if( rcp_errors()->get_error_data( $code ) == $error_id ) {
+
+				$message = rcp_errors()->get_error_message( $code );
+
+				$html .= '<p class="rcp_error ' . esc_attr( $code ) . '"><span>' . $message . '</span></p>';
+
+			}
+
+		}
+
+		$html .= '</div>';
+
+	}
+
+	return apply_filters( 'rcp_error_messages_html', $html, $errors );
+
+}
+
+/**
+ * Displays the HTML for error messages
+ *
+ * @access      public
+ * @since       1.0
+ */
+function rcp_show_error_messages( $error_id = '' ) {
+	if( rcp_errors()->get_error_codes() ) {
+		do_action( 'rcp_errors_before' );
+		echo rcp_get_error_messages_html();
+		do_action( 'rcp_errors_after' );
+	}
 }
