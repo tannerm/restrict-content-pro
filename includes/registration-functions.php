631b2d7c752fe2225fc87a9b713c66ae123d9f86
Pippin Williamson <pippin@pippinsplugins.com>
2015-04-10 21:16:33 -0500

    Add rcp_discounts_once_per_user filter

--- includes/registration-functions.php
+++ includes/registration-functions.php
@@ -66,7 +66,7 @@ function rcp_process_registration() {
 				// the entered discount code is incorrect
 				rcp_errors()->add( 'invalid_discount', __( 'The discount you entered is invalid', 'rcp' ), 'register' );
 			}
-			if( ! $user_data['need_new'] && rcp_user_has_used_discount( $user_data['id'] , $discount ) ) {
+			if( ! $user_data['need_new'] && rcp_user_has_used_discount( $user_data['id'] , $discount ) && apply_filters( 'rcp_discounts_once_per_user', true ) ) {
 				rcp_errors()->add( 'discount_already_used', __( 'You can only use the discount code once', 'rcp' ), 'register' );
 			}
 		}
