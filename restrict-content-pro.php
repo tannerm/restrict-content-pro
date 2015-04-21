26d5888c5efd90df9eec79495eb35c1d388b46f3
Pippin Williamson <pippin@pippinsplugins.com>
2015-04-21 10:29:57 -0500

    Add PHP 5.3 requirement check

--- restrict-content-pro.php
+++ restrict-content-pro.php
@@ -128,92 +128,106 @@ function rcp_load_textdomain() {
 }
 add_action( 'init', 'rcp_load_textdomain' );
 
-
 /*******************************************
-* file includes
+* requirement checks
 *******************************************/
 
+if( version_compare( PHP_VERSION, '5.3', '<' ) ) {
 
-// global includes
-require( RCP_PLUGIN_DIR . 'includes/install.php' );
-include( RCP_PLUGIN_DIR . 'includes/class-rcp-capabilities.php' );
-include( RCP_PLUGIN_DIR . 'includes/class-rcp-levels.php' );
-include( RCP_PLUGIN_DIR . 'includes/class-rcp-member.php' );
-include( RCP_PLUGIN_DIR . 'includes/class-rcp-payments.php' );
-include( RCP_PLUGIN_DIR . 'includes/class-rcp-discounts.php' );
-include( RCP_PLUGIN_DIR . 'includes/scripts.php' );
-include( RCP_PLUGIN_DIR . 'includes/ajax-actions.php' );
-include( RCP_PLUGIN_DIR . 'includes/cron-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/deprecated/functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/discount-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/email-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-manual.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-paypal.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-stripe.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateways.php' );
-include( RCP_PLUGIN_DIR . 'includes/gateways/gateway-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/invoice-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/login-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/member-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/misc-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/registration-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/subscription-functions.php' );
-include( RCP_PLUGIN_DIR . 'includes/error-tracking.php' );
-include( RCP_PLUGIN_DIR . 'includes/shortcodes.php' );
-include( RCP_PLUGIN_DIR . 'includes/template-functions.php' );
-
-if( !class_exists( 'WP_Logging' ) ) {
-	include( RCP_PLUGIN_DIR . 'includes/libraries/class-wp-logging.php' );
-}
-
-// admin only includes
-if( is_admin() ) {
-
-	include( RCP_PLUGIN_DIR . 'includes/upgrades.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/admin-pages.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/admin-notices.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/admin-ajax-actions.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/screen-options.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/members/members-page.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/settings/settings.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/subscriptions/subscription-levels.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/discounts/discount-codes.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/payments/payments-page.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/reports/reports-page.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/export.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/logs.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/help/help-menus.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/help/help-page.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/metabox.php' );
-	include( RCP_PLUGIN_DIR . 'includes/admin/categories.php' );
-	include( RCP_PLUGIN_DIR . 'includes/user-page-columns.php' );
-	include( RCP_PLUGIN_DIR . 'includes/process-data.php' );
-	include( RCP_PLUGIN_DIR . 'includes/export-functions.php' );
-	include( RCP_PLUGIN_DIR . 'RCP_Plugin_Updater.php' );
-
-	// retrieve our license key from the DB
-	$license_key = ! empty( $rcp_options['license_key'] ) ? trim( $rcp_options['license_key'] ) : false;
-
-	if( $license_key ) {
-		// setup the updater
-		$rcp_updater = new RCP_Plugin_Updater( 'https://pippinsplugins.com', RCP_PLUGIN_FILE, array(
-				'version' 	=> RCP_PLUGIN_VERSION, // current version number
-				'license' 	=> $license_key, // license key (used get_option above to retrieve from DB)
-				'item_id'   => 7460, // Download ID
-				'author' 	=> 'Pippin Williamson' // author of this plugin
-			)
-		);
+	add_action( 'admin_notices', 'rcp_below_php_version_notice' );
+	function rcp_below_php_version_notice() {
+		echo '<div class="error"><p>' . __( 'Your version of PHP is below the minimum version of PHP required by Restrict Content Pro. Please contact your host and request that your version be upgraded to 5.3 or later.', 'rcp' ) . '</p></div>';
 	}
 
 } else {
 
-	include( RCP_PLUGIN_DIR . 'includes/member-forms.php' );
-	include( RCP_PLUGIN_DIR . 'includes/content-filters.php' );
-	include( RCP_PLUGIN_DIR . 'includes/feed-functions.php' );
-	if( isset( $rcp_options['enable_recaptcha'] ) ) {
-		require_once( RCP_PLUGIN_DIR . 'includes/captcha-functions.php' );
+
+	/*******************************************
+	* file includes
+	*******************************************/
+
+	// global includes
+	require( RCP_PLUGIN_DIR . 'includes/install.php' );
+	include( RCP_PLUGIN_DIR . 'includes/class-rcp-capabilities.php' );
+	include( RCP_PLUGIN_DIR . 'includes/class-rcp-levels.php' );
+	include( RCP_PLUGIN_DIR . 'includes/class-rcp-member.php' );
+	include( RCP_PLUGIN_DIR . 'includes/class-rcp-payments.php' );
+	include( RCP_PLUGIN_DIR . 'includes/class-rcp-discounts.php' );
+	include( RCP_PLUGIN_DIR . 'includes/scripts.php' );
+	include( RCP_PLUGIN_DIR . 'includes/ajax-actions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/cron-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/deprecated/functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/discount-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/email-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-manual.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-paypal.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateway-stripe.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/class-rcp-payment-gateways.php' );
+	include( RCP_PLUGIN_DIR . 'includes/gateways/gateway-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/invoice-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/login-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/member-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/misc-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/registration-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/subscription-functions.php' );
+	include( RCP_PLUGIN_DIR . 'includes/error-tracking.php' );
+	include( RCP_PLUGIN_DIR . 'includes/shortcodes.php' );
+	include( RCP_PLUGIN_DIR . 'includes/template-functions.php' );
+
+	if( !class_exists( 'WP_Logging' ) ) {
+		include( RCP_PLUGIN_DIR . 'includes/libraries/class-wp-logging.php' );
 	}
-	include( RCP_PLUGIN_DIR . 'includes/query-filters.php' );
-	include( RCP_PLUGIN_DIR . 'includes/redirects.php' );
+
+	// admin only includes
+	if( is_admin() ) {
+
+		include( RCP_PLUGIN_DIR . 'includes/upgrades.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/admin-pages.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/admin-notices.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/admin-ajax-actions.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/screen-options.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/members/members-page.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/settings/settings.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/subscriptions/subscription-levels.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/discounts/discount-codes.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/payments/payments-page.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/reports/reports-page.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/export.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/logs.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/help/help-menus.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/help/help-page.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/metabox.php' );
+		include( RCP_PLUGIN_DIR . 'includes/admin/categories.php' );
+		include( RCP_PLUGIN_DIR . 'includes/user-page-columns.php' );
+		include( RCP_PLUGIN_DIR . 'includes/process-data.php' );
+		include( RCP_PLUGIN_DIR . 'includes/export-functions.php' );
+		include( RCP_PLUGIN_DIR . 'RCP_Plugin_Updater.php' );
+
+		// retrieve our license key from the DB
+		$license_key = ! empty( $rcp_options['license_key'] ) ? trim( $rcp_options['license_key'] ) : false;
+
+		if( $license_key ) {
+			// setup the updater
+			$rcp_updater = new RCP_Plugin_Updater( 'https://pippinsplugins.com', RCP_PLUGIN_FILE, array(
+					'version' 	=> RCP_PLUGIN_VERSION, // current version number
+					'license' 	=> $license_key, // license key (used get_option above to retrieve from DB)
+					'item_id'   => 7460, // Download ID
+					'author' 	=> 'Pippin Williamson' // author of this plugin
+				)
+			);
+		}
+
+	} else {
+
+		include( RCP_PLUGIN_DIR . 'includes/member-forms.php' );
+		include( RCP_PLUGIN_DIR . 'includes/content-filters.php' );
+		include( RCP_PLUGIN_DIR . 'includes/feed-functions.php' );
+		if( isset( $rcp_options['enable_recaptcha'] ) ) {
+			require_once( RCP_PLUGIN_DIR . 'includes/captcha-functions.php' );
+		}
+		include( RCP_PLUGIN_DIR . 'includes/query-filters.php' );
+		include( RCP_PLUGIN_DIR . 'includes/redirects.php' );
+	}
+
 }
