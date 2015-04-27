f1983081964e93dc2c15b484c78136a84194051f
Pippin Williamson <pippin@pippinsplugins.com>
2015-04-27 09:42:20 -0500

    Remove double esc

--- includes/admin/payments/payments-page.php
+++ includes/admin/payments/payments-page.php
@@ -118,7 +118,7 @@ function rcp_payments_page() {
 										<span>&nbsp;|&nbsp;</span>
 										<a href="<?php echo esc_url( add_query_arg( array( 'payment_id' => $payment->id, 'view' => 'edit-payment' ) ) ); ?>" class="rcp-edit-payment"><?php _e( 'Edit', 'rcp' ); ?></a>
 										<span>&nbsp;|&nbsp;</span>
-										<a href="<?php echo wp_nonce_url( esc_url( add_query_arg( array( 'payment_id' => $payment->id, 'rcp-action' => 'delete_payment' ) ) ), 'rcp_delete_payment_nonce' ); ?>" class="rcp-delete-payment"><?php _e( 'Delete', 'rcp' ); ?></a>
+										<a href="<?php echo wp_nonce_url( add_query_arg( array( 'payment_id' => $payment->id, 'rcp-action' => 'delete_payment' ) ), 'rcp_delete_payment_nonce' ); ?>" class="rcp-delete-payment"><?php _e( 'Delete', 'rcp' ); ?></a>
 									</td>
 								<?php endif; ?>
 							</tr>
