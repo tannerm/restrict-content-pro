1dcd1280eb577d31437b6f504b0dbfb59f9829c4
Pippin Williamson <pippin@pippinsplugins.com>
2015-06-26 11:02:05 -0500

    Fix missing  that caused errors on login form to not be shown

--- includes/error-tracking.php
+++ includes/error-tracking.php
@@ -55,7 +55,7 @@ function rcp_get_error_messages_html( $error_id = '' ) {
 function rcp_show_error_messages( $error_id = '' ) {
 	if( rcp_errors()->get_error_codes() ) {
 		do_action( 'rcp_errors_before' );
-		echo rcp_get_error_messages_html();
+		echo rcp_get_error_messages_html( $error_id );
 		do_action( 'rcp_errors_after' );
 	}
 }
