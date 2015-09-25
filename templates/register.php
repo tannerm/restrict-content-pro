6ac0e91b822019b87ea134d06d2ae928fe9f32c8
Pippin Williamson <pippin@pippinsplugins.com>
2015-09-25 15:47:27 -0500

    Fix translation of log in to renew string

--- templates/register.php
+++ templates/register.php
@@ -18,7 +18,7 @@ rcp_show_error_messages( 'register' ); ?>
 	<?php if( ! is_user_logged_in() ) { ?>
 
 	<div class="rcp_login_link">
-		<p><?php _e( sprintf( '<a href="%s">Log in</a> if you wish to renew an existing subscription.', rcp_get_login_url( rcp_get_current_url() ) ), 'rcp' ); ?></p>
+		<p><?php printf( __( '<a href="%s">Log in</a> if you wish to renew an existing subscription.', 'rcp' ), rcp_get_login_url( rcp_get_current_url() ) ); ?></p>
 	</div>
 
 	<?php do_action( 'rcp_before_register_form_fields' ); ?>
