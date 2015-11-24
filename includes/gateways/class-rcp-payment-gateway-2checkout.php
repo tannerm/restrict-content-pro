8917a3300cdd0f0ca8a0d35d7e7ba2ec4f824b4c
Pippin Williamson <pippin@pippinsplugins.com>
2015-11-24 10:26:59 -0600

    Resolve undefined index twocheckout_secret_word

--- includes/gateways/class-rcp-payment-gateway-2checkout.php
+++ includes/gateways/class-rcp-payment-gateway-2checkout.php
@@ -29,7 +29,7 @@ class RCP_Payment_Gateway_2Checkout extends RCP_Payment_Gateway {
 		$this->supports[]  = 'recurring';
 		$this->supports[]  = 'fees';
 
-		$this->secret_word = isset( $rcp_options['sandbox'] ) ? trim( $rcp_options['twocheckout_secret_word'] ) : '';;
+		$this->secret_word = isset( $rcp_options['twocheckout_secret_word'] ) ? trim( $rcp_options['twocheckout_secret_word'] ) : '';
 		$this->test_mode   = isset( $rcp_options['sandbox'] );
 
 		if( $this->test_mode ) {
