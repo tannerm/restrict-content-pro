8a7fc8d65445a5f412392ea8592cd399d13744ac
John Parris <public@johnparris.com>
2017-01-02 19:40:17 -0500

    Remove role given by subscription when member expires. #427 (#834)
    * Remove role given by subscription when member expires. #427
    
    * Check for subscription on status change. #427

--- includes/member-functions.php
+++ includes/member-functions.php
@@ -1378,3 +1378,24 @@ function rcp_maybe_disable_toolbar() {
 	}
 }
 add_action( 'init', 'rcp_maybe_disable_toolbar', 9999 );
+
+/**
+ * Removes the subscription-assigned role from a member when the member expires.
+ *
+ * @since 2.7
+ */
+function rcp_update_expired_member_role( $status, $member_id, $old_status, $member ) {
+
+	if ( 'expired' !== $status ) {
+		return;
+	}
+
+	$subscription = rcp_get_subscription_details( $member->get_subscription_id() );
+
+	$default_role = get_option( 'default_role', 'subscriber' );
+
+	if ( ! empty( $subscription ) && is_object( $subscription ) && $subscription->role !== $default_role ) {
+		$member->remove_role( $subscription->role );
+	}
+}
+add_action( 'rcp_set_status', 'rcp_update_expired_member_role', 10, 4 );
