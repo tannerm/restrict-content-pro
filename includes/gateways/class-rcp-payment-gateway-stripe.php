e3771ea16aded457cb22173d7c87f1362058da03
John Parris <public@johnparris.com>
2017-01-24 12:58:39 -0500

    WIP: Adjust Stripe webhook event logic for trial. #335

--- includes/gateways/class-rcp-payment-gateway-stripe.php
+++ includes/gateways/class-rcp-payment-gateway-stripe.php
@@ -472,17 +472,18 @@ class RCP_Payment_Gateway_Stripe extends RCP_Payment_Gateway {
 
 							}
 
-						} elseif ( $event->type == 'invoice.payment_succeeded' ) {
+						} elseif ( $event->type == 'invoice.payment_succeeded' && empty( $payment_event->charge ) ) {
 
 							$invoice = $payment_event;
 
-							// Successful subscription paid made with account credit where no charge is created
-							if ( empty( $payment_event->charge ) ) {
-								$payment_data['amount']         = $payment_event->amount_due / rcp_stripe_get_currency_multiplier();
-								$payment_data['transaction_id'] = $payment_event->id;
+							// Successful subscription paid made with account credit, or free trial, where no charge is created
+							if ( 'in_' !== substr( $payment_event->id, 0, 3 ) ) {
+								$payment_data['amount']         = $invoice->amount_due / rcp_stripe_get_currency_multiplier();
+								$payment_data['transaction_id'] = $invoice->id;
 							} else {
-								$payment_data['amount']         = $payment_event->lines->data[0]->amount / rcp_stripe_get_currency_multiplier();
-								$payment_data['transaction_id'] = $payment_event->charge;
+								$payment_data['amount']           = $invoice->lines->data[0]->amount / rcp_stripe_get_currency_multiplier();
+								$payment_data['transaction_id']   = $invoice->subscription; // trials don't get a charge ID. set the subscription ID.
+								$payment_data['is_trial_invoice'] = true;
 							}
 
 						}
@@ -509,8 +510,10 @@ class RCP_Payment_Gateway_Stripe extends RCP_Payment_Gateway {
 							$payment_data['subscription']     = $member->get_subscription_name();
 							$payment_data['subscription_key'] = $member->get_subscription_key();
 
-							// record this payment if it hasn't been recorded yet
-							$rcp_payments->insert( $payment_data );
+							// record this payment if it hasn't been recorded yet and it's not a trial invoice
+							if ( empty( $payment_data['is_trial_invoice'] ) ) {
+								$rcp_payments->insert( $payment_data );
+							}
 
 							do_action( 'rcp_stripe_charge_succeeded', $user, $payment_data );
 
