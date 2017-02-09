91fc7ca8bafbc6834c7d911b3df28e0a8a54c9b8
Ashley Evans <ashley@nosegraze.com>
2017-02-09 13:54:17 +0000

    Set display name to username if first and last name fields are empty.

--- includes/registration-functions.php
+++ includes/registration-functions.php
@@ -140,7 +140,7 @@ function rcp_process_registration() {
 				'user_email'      => $user_data['email'],
 				'first_name'      => $user_data['first_name'],
 				'last_name'       => $user_data['last_name'],
-				'display_name'    => $user_data['first_name'] . ' ' . $user_data['last_name'],
+				'display_name'    => ! empty( trim( $user_data['first_name'] . ' ' . $user_data['last_name'] ) ) ? $user_data['first_name'] . ' ' . $user_data['last_name'] : $user_data['login'],
 				'user_registered' => date( 'Y-m-d H:i:s' )
 			)
 		);
