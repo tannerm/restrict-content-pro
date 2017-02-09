5806420eba64119853814f2906443f989ac4960a
Ashley Evans <ashley@nosegraze.com>
2017-02-09 12:18:31 +0000

    Remove brackets from lost password email link.

--- includes/login-functions.php
+++ includes/login-functions.php
@@ -281,7 +281,7 @@ function rcp_retrieve_password() {
 	$message .= sprintf(__('Username: %s', 'rcp'), $user_login) . "\r\n\r\n";
 	$message .= __('If this was a mistake, just ignore this email and nothing will happen.', 'rcp') . "\r\n\r\n";
 	$message .= __('To reset your password, visit the following address:', 'rcp') . "\r\n\r\n";
-	$message .= '<' . esc_url_raw( add_query_arg( array( 'rcp_action' => 'lostpassword_reset', 'key' => $key, 'login' => rawurlencode( $user_login ) ), $_POST['rcp_redirect'] ) ) . ">\r\n";
+	$message .= esc_url_raw( add_query_arg( array( 'rcp_action' => 'lostpassword_reset', 'key' => $key, 'login' => rawurlencode( $user_login ) ), $_POST['rcp_redirect'] ) ) . "\r\n";
 
 	if ( is_multisite() ) {
 
