d94a6fdcb213f83ace0e80686e767d13c349412c
John Parris <public@johnparris.com>
2017-02-14 09:01:34 -0500

    Bump version to 2.7.2

--- restrict-content-pro.php
+++ restrict-content-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: Restrict Content Pro
  * Plugin URL: https://restrictcontentpro.com
  * Description: Set up a complete subscription system for your WordPress site and deliver premium content to your subscribers. Unlimited subscription packages, membership management, discount codes, registration / login forms, and more.
- * Version: 2.7.1
+ * Version: 2.7.2
  * Author: Restrict Content Pro Team
  * Author URI: https://restrictcontentpro.com
  * Contributors: mordauk
@@ -20,7 +20,7 @@ if ( !defined( 'RCP_PLUGIN_FILE' ) ) {
 	define( 'RCP_PLUGIN_FILE', __FILE__ );
 }
 if ( !defined( 'RCP_PLUGIN_VERSION' ) ) {
-	define( 'RCP_PLUGIN_VERSION', '2.7.1' );
+	define( 'RCP_PLUGIN_VERSION', '2.7.2' );
 }
 if ( ! defined( 'CAL_GREGORIAN' ) ) {
 	define( 'CAL_GREGORIAN', 1 );
