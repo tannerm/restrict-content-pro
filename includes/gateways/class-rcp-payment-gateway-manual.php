d3377a69f5de4dba60fea6a4dd93cb4f8ef22f6a
Ashley Evans <ashley@nosegraze.com>
2017-02-15 15:19:40 +0000

    EMail admin when manual payment is received.

--- includes/gateways/class-rcp-payment-gateway-manual.php
+++ includes/gateways/class-rcp-payment-gateway-manual.php
@@ -20,8 +20,6 @@ class RCP_Payment_Gateway_Manual extends RCP_Payment_Gateway {
 	 */
 	public function init() {
 
-		global $rcp_options;
-
 		$this->supports[]  = 'one-time';
 		$this->supports[]  = 'fees';
 
@@ -35,6 +33,8 @@ class RCP_Payment_Gateway_Manual extends RCP_Payment_Gateway {
 	 * @return void
 	 */
 	public function process_signup() {
+
+		global $rcp_options;
 
 		$member = new RCP_Member( $this->user_id );
 
@@ -59,7 +59,27 @@ class RCP_Payment_Gateway_Manual extends RCP_Payment_Gateway {
 		);
 
 		$rcp_payments = new RCP_Payments();
-		$rcp_payments->insert( $payment_data );
+		$payment_id   = $rcp_payments->insert( $payment_data );
+
+		// Email site admin about the payment.
+		if ( ! isset( $rcp_options['disable_new_user_notices'] ) ) {
+			$admin_emails   = array();
+			$admin_emails[] = get_option( 'admin_email' );
+			$admin_emails   = apply_filters( 'rcp_admin_notice_emails', $admin_emails );
+
+			$emails             = new RCP_Emails;
+			$emails->member_id  = $this->user_id;
+			$emails->payment_id = $payment_id;
+
+			$site_name = stripslashes_deep( html_entity_decode( get_bloginfo( 'name' ), ENT_COMPAT, 'UTF-8' ) );
+
+			$admin_message = __( 'Hello', 'rcp' ) . "\n\n" . $member->display_name . ' (' . $member->user_login . ') ' . __( 'just submitted a manual payment on', 'rcp' ) . ' ' . $site_name . ".\n\n" . __( 'Subscription level', 'rcp' ) . ': ' . $member->get_subscription_name() . "\n\n";
+			$admin_message = apply_filters( 'rcp_before_admin_email_manual_payment_thanks', $admin_message, $this->user_id );
+			$admin_message .= __( 'Thank you', 'rcp' );
+			$admin_subject = sprintf( __( 'New manual payment on %s', 'rcp' ), $site_name );
+
+			$emails->send( $admin_emails, $admin_subject, $admin_message );
+		}
 
 		wp_redirect( $this->return_url ); exit;
 
