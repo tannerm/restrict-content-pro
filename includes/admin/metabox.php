5b29bb37d29cd1fe6a89dfa63a85c3cd82a4338c
Ashley Evans <ashley@nosegraze.com>
2017-02-21 12:40:47 +0000

    Delete meta if values are empty.

--- includes/admin/metabox.php
+++ includes/admin/metabox.php
@@ -211,11 +211,17 @@ function rcp_save_meta_data( $post_id ) {
 	$hide_in_feed      = isset( $_POST['rcp_hide_from_feed'] );
 	$user_role         = sanitize_text_field( $_POST[ 'rcp_user_level' ] );
 
-	if ( 'individual' === $content_excerpts ) {
+	if ( 'individual' === $content_excerpts && $show_excerpt ) {
 		update_post_meta( $post_id, 'rcp_show_excerpt', $show_excerpt );
+	} else {
+		delete_post_meta( $post_id, 'rcp_show_excerpt' );
 	}
 
-	update_post_meta( $post_id, 'rcp_hide_from_feed', $hide_in_feed );
+	if ( $hide_in_feed ) {
+		update_post_meta( $post_id, 'rcp_hide_from_feed', $hide_in_feed );
+	} else {
+		delete_post_meta( $post_id, 'rcp_hide_from_feed' );
+	}
 
 	if ( 'unrestricted' !== $_POST['rcp_restrict_by'] ) {
 		update_post_meta( $post_id, 'rcp_user_level', $user_role );
