d4eb45d77ff12af6cd037f5b666286bdcff55a83
Ashley Evans <ashley@nosegraze.com>
2017-04-12 13:54:49 +0100

    Add header attributes to register form shortcode. #677

--- templates/register-single.php
+++ templates/register-single.php
@@ -14,7 +14,7 @@
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 
-global $rcp_options, $rcp_level, $post, $rcp_levels_db;
+global $rcp_options, $rcp_level, $post, $rcp_levels_db, $rcp_register_form_atts;
 
 $level = rcp_get_subscription_details( $rcp_level );
 $discount = ! empty( $_REQUEST['discount'] ) ? sanitize_text_field( $_REQUEST['discount'] ) : '';
@@ -23,11 +23,11 @@ $has_trial = $rcp_levels_db->has_trial( $level->id );
 
 <?php if( ! is_user_logged_in() ) { ?>
 	<h3 class="rcp_header">
-		<?php echo apply_filters( 'rcp_registration_header_logged_in', __( 'Register New Account', 'rcp' ) ); ?>
+		<?php echo apply_filters( 'rcp_registration_header_logged_in', $rcp_register_form_atts['logged_out_header'] ); ?>
 	</h3>
 <?php } else { ?>
 	<h3 class="rcp_header">
-		<?php echo apply_filters( 'rcp_registration_header_logged_out', __( 'Upgrade Your Subscription', 'rcp' ) ); ?>
+		<?php echo apply_filters( 'rcp_registration_header_logged_out', $rcp_register_form_atts['logged_in_header'] ); ?>
 	</h3>
 <?php }
 
